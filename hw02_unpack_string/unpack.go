dab23db0d4d48b618f89ad2008e52bd0ad3ef264
wacker <wackeryi@gmail.com>
2026-09-22 17:15:56 +0500

    UPD доп задание

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,26 +8,28 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-	// финальная строка
 	var result strings.Builder
-
-	// например 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
-	// Разбиваем строку по символам
 	runes := []rune(str)
 
 	for i := 0; i < len(runes); i++ {
 		current := runes[i]
-		repeat := 1
 
-		// Проверка что цифры не удиут сразу или не 2 раза подряд
 		if IsDigit(current) {
 			return "", ErrInvalidString
 		}
 
-		// проверяем, есть ли слудующий символ
+		if current == '\\' {
+			if i+1 >= len(runes) || (!IsDigit(runes[i+1]) && runes[i+1] != '\\') {
+				return "", ErrInvalidString
+			}
+
+			current = runes[i+1]
+			i++
+		}
+
+		repeat := 1
 		if i+1 < len(runes) && IsDigit(runes[i+1]) {
 			repeat = int(runes[i+1] - '0')
-			// Пропускаем цифру в след иттерации
 			i++
 		}
 
