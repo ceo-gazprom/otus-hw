d12dfacf731a6e0aa195e963bcf8cc2549e258c8
wacker <wackeryi@gmail.com>
2026-09-22 18:22:51 +0500

    ADD сделана сортировка

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	// Делим поп робелам, переносам и тдж
+	parts := strings.Fields(text)
+	words := make(map[string]int)
+
+	// Получаем кол-во использований для каждого слова
+	for _, word := range parts {
+		words[word]++
+	}
+
+	// Делаем слайс для сортировки
+	result := make([]string, 0, len(words))
+
+	for word := range words {
+		result = append(result, word)
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		// если потворения одинаковы
+		if words[result[i]] == words[result[j]] {
+			// по алфавиту
+			return result[i] < result[j]
+		}
+
+		return words[result[i]] > words[result[j]]
+	})
+
+	// Возвращаем 10
+	if len(result) > 10 {
+		result = result[:10]
+	}
+
+	return result
 }
